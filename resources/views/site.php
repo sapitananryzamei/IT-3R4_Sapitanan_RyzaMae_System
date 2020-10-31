795383266ecc9d90917342c79de5415cfca8f7bc
RyzaMei <jayannarquiza0824@gmail.com>
2020-10-31 10:21:38 +0800

    nextcommit

--- resources/views/site.php
+++ resources/views/site.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<style>
             body{
-                background-color: red;
+                background-color: grey;
                 margin:0;
                 background-size: 100%;
                 background-attachment: scroll;
